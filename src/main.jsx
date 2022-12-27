d6a41ef4202689a316680e15cfbe3b20de99fd28
gabrieldezena10 <gabriel.deztor@gmail.com>
2022-12-26 23:42:35 -0300

    feat: implement context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import StepContextProvider from "./context/StepContext"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StepContextProvider>
+      <App />
+    </StepContextProvider>
   </React.StrictMode>,
 )
